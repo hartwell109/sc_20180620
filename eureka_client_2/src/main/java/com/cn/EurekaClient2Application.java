ce763e4cbcaf1ca7b1fdccfa922c927ffdef593f
hartwell109 <hartwell@sohu.com>
2018-06-24 07:32:26 +0800

    20170624

---
+++ eureka_client_2/src/main/java/com/cn/EurekaClient2Application.java
@@ -0,0 +1,12 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClient2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClient2Application.class, args);
+	}
+}
---
+++ eureka_server_1/src/main/java/com/cn/EurekaServer1Application.java
@@ -0,0 +1,12 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer1Application.class, args);
+	}
+}
